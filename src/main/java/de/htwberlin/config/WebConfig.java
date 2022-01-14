2bdf97b75c7e1d824267708681c8518506709e5f
Daniel Wagner <wagner.daniel87@gmail.com>
2022-01-14 18:50:40 +0100

    add webconfig

---
+++ src/main/java/de/htwberlin/config/WebConfig.java
@@ -0,0 +1,20 @@
+package de.htwberlin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+            .allowedMethods("*")
+            .allowedOrigins(
+                "http://localhost:3000"
+            );
+    }
+}
